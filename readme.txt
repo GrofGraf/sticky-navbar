7c91f499eb1958ff6be0d440ff0807c879cf4ae3
Admin <Admin>
2017-01-30 11:28:08 +0100

    initial commit

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Sticky navbar container fixed to top.
+
+It hides on scroll down and shows on scroll up.
---
+++ sticky-nav.js
@@ -0,0 +1,20 @@
+var navbar = document.getElementById("sticky-navbar");
+var lastPosition = window.pageYOffset;
+window.onscroll = function(){
+	var top = navbar.getBoundingClientRect().top;
+	if(lastPosition > window.pageYOffset){
+		if(top + (lastPosition - window.pageYOffset) < 0){
+			navbar.style.top = top + (lastPosition - window.pageYOffset)
+		}else{
+			navbar.style.top = 0;
+		}
+		console.log("scroll top");
+	}else{
+		if(top + (lastPosition - window.pageYOffset) > -60){
+			navbar.style.top = top + (lastPosition - window.pageYOffset)
+		}else{
+			navbar.style.top = -60;
+		}
+	}
+	lastPosition = window.pageYOffset;
+}
